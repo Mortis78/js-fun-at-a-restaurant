2627027505ab894e5835303b07bf9dced046ab35
John Ammon <ammonj78@gmail.com>
2022-10-13 09:11:51 -0600

    Adds my recipe

--- src/meal.js
+++ src/meal.js
@@ -4,12 +4,25 @@ function nameMenuItem() {
 
 
 module.exports = {
-  nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
 
 
+  name:'curry' ,
+  Ingredients: ['tomatos','onions','garlic','ginger','curry powder ','chickon','heavy cream'],
+  Price:12,
+  Recipe:{
+
+    step1:'carmolise onions in pan',
+    step2:'add garlic finely chopped,cook for 5 min ',
+    step3:'add ginger',
+    step4:'add diced tomatos, cook for 2 hours on low simmer stiring accasionally',
+    step5:'add half of your curry powder',
+    step6:'add your chickon and cook for another hour',
+    step7:'add the other half of your curry powder and cook for another hour ',
+    step8:'add heavy cream',
+    step9:'serve hot and enjoy',
+  }
+
+// decreasePrice,
+
+};
+console.log(module.exports)
