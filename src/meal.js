63046ba2fc5f9354ef08478d2acc62c1d0c3e10c
John Ammon <ammonj78@gmail.com>
2022-10-14 09:29:32 -0600

    Passes test 1-9

--- src/meal.js
+++ src/meal.js
@@ -4,17 +4,34 @@ return 'Delicious ' + food
 
 };
 
-function createMenuItem()
+function createMenuItem(name, price, type)
 {
 return {
-  food, price, type
-}
+  //name, price, type
+  //
+  name: name ,
+  price: price ,
+  type: type ,
+
+};
 
 }
+function addIngredients(food,foodArray){
+
+//i need to check each itom in the array for the new itom passing through, if the itom has a duplecet dont add new itom.
+
+if (foodArray.includes(food)){
+
+ }
+else {
+
+  return foodArray.push(food)
+}
+ }
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  // addIngredients,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
