0230a003b87efeadf0c056ffaad8168092a041c6
John Ammon <ammonj78@gmail.com>
2022-10-14 21:01:49 -0600

    saves progres on orders.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,23 @@
 
+function takeOrder(orderObject, deliveryArray){
+  if (deliveryArray.length < 3){
+    return deliveryArray.push(orderObject)
+
+  }
+
+}
+
+function refundOrder(orderNum,deliveryArray){
+  // if(orderNum.)
+
+
+
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
