edc9bc04562fb43c8cf8cc2380fbdffcd6c162db
John Ammon <ammonj78@gmail.com>
2022-10-16 16:45:29 -0600

    Complets orders

--- src/order.js
+++ src/order.js
@@ -2,51 +2,38 @@
 function takeOrder(orderObject, deliveryArray){
   if (deliveryArray.length < 3){
     return deliveryArray.push(orderObject)
-
   }
-
 }
 
 function refundOrder(orderNum,deliveryArray){
-
 for (var i = 0; i < deliveryArray.length; i++) {
 if (deliveryArray[i].orderNumber === orderNum){
-
 deliveryArray.splice(i, 1);
   }
-
 }
 return deliveryArray
 }
 
+
 function listItems(deliveryArray){
-
   var items = []
-
    for (var i = 0; i < deliveryArray.length; i++){
-
-    // push each item into the items foodArray
     items.push(deliveryArray[i].item)
    }
-
 return items.join(', ')
-
-
 }
 
 function searchOrder(deliveryArray,item){
-  var inList = null
+  var inList = false
   for (var i = 0; i < deliveryArray.length; i++){
     if (deliveryArray[i].item === item){
       inList = true;
-    }else {
-      inList = false
     }
-
   }
 return inList
-
 }
+
+
 
 module.exports = {
 
