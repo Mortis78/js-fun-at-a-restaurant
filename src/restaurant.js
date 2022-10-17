a7dc9ec2a71a54547f9b2f855ddab046cc70b15a
John Ammon <ammonj78@gmail.com>
2022-10-16 19:21:33 -0600

    Saves progres on restaurants

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,80 @@
+function createRestaurant(restaurantName){
+  var restaurant = {
+    name:restaurantName,
+    menus: {
+      breakfast:[],
+      lunch:[],
+      dinner:[]
+  }
+}
+return restaurant
+}
+function addMenuItem(restaurant,menueItem){
+
+  //restaurant.menus.lunch.push(foodItem);
+  if (menueItem.type === "breakfast" && !restaurant.menus.breakfast.includes(menueItem)) {
+    restaurant.menus.breakfast.push(menueItem)
+
+  } else if (menueItem.type === "lunch" && !restaurant.menus.lunch.includes(menueItem)) {
+    restaurant.menus.lunch.push(menueItem)
+
+  } else if (foodItem.type === "dinner" && !restaurant.menus.dinner.includes(menueItem)) {
+    restaurant.menus.dinner.push(menueItem)
+  };
+  return restaurant
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   for (var i = 0; i < restaurant.menus.lunch.length; i++){
+// //  console.log(' first step')
+//     if (menueItem.type === 'lunch' &&
+//     restaurant.menus.lunch[i].name !== menueItem.name){
+//       console.log('step two')
+// //      restaurant.menus.lunch.push(menueItem)
+//     }
+//       console.log(menueItem)
+// }
+//
+// //   for (var i = 0; i < restaurant.menus.breakfast.length; i++){
+// //
+// //     if (menueItem.type === 'breakfast' &&
+// //     !restaurant.menus.breakfast[i].name !== menueItem.name){
+// //      restaurant.menus.breakfast.push(menueItem)
+// // }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
